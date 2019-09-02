59680e38a6c6e0923cf1edf36c57c7710d6bff50
Manuel Riquelme <clonbg@gmail.com>
2019-09-02 16:25:59 +0200

    datos entre componentes

--- src/components/Busqueda.vue
+++ src/components/Busqueda.vue
@@ -1,26 +1,35 @@
 <template>
-    <div id="busqueda">
-        <form>
-            <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-5 col-form-label">Email</label>
-                <div class="col-sm-5">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                </div>
-            </div>
-        </form>
-        {{ $data }}
-
-    </div>
+  <div id="busqueda">
+    <form>
+      <div class="form-group row">
+        <label for="inputEmail3" class="col-sm-5 col-form-label">Email</label>
+        <div class="col-sm-5">
+          <input type="email" @keyup="addBusqueda" v-model="stringBuscar" class="form-control" id="inputEmail3" placeholder="Email" />
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+import EventBus from "../bus";
 export default {
-    name:'busqueda',
-}
+  name: "busqueda",
+  data() {
+    return {
+      stringBuscar: ""
+    };
+  },
+  methods: {
+    addBusqueda: function() {
+      EventBus.$emit("add-busqueda", this.stringBuscar);
+    }
+  }
+};
 </script>
 
 <style>
-    form{
-        align-content: center
-    }
+form {
+  align-content: center;
+}
 </style>
--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -1,62 +1,67 @@
 <template>
-    <div id="usuarios">
-        <ul>
-            <div v-for='user in users' :key='user.name'>
-                <div class="float-left card text-white bg-success mb-3 tamano-tarjetas">
-                    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                        <div class="card-header">{{ user.name.title }}</div>
-                        <div class="card-body">
-                            <h5 class="card-title">{{ user.name.first }} {{ user.name.last }}</h5>
-                            <p class="card-text">
-                                <img :src="user.picture.large" :alt="user.name-first"/>>
-                                <div class="card-footer"><small>{{ user.email }}</small></div>
-                            
-                        </div>
-                    </div>
-                </div>
+  <div id="usuarios">
+    <ul>
+      <div v-for="user in users" :key="user.name.first">
+        <div class="float-left card text-white bg-success mb-3 tamano-tarjetas">
+          <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+            <div class="card-header">{{ user.name.title }}</div>
+            <div class="card-body">
+              <h5 class="card-title">{{ user.name.first }} {{ user.name.last }}</h5>
+              <p class="card-text">
+                <img :src="user.picture.large" :alt="user.name.first" />>
+              </p>
+              <div class="card-footer">
+                <small>{{ user.email }}</small>
+              </div>
             </div>
-            {{ buscar }}
-        </ul>
-        <div>
+          </div>
         </div>
-        
-        </div>
+      </div>
+    </ul>
+    <div></div>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
-    export default {
-        name: 'usuarios',
-        data() {
-            return {
-                users: [],
-                buscar:''
-            }
-        },
-        computed: {
-            filtrados: function() {
-                return this.users.filter((user) => {
-                    return user.name.first.includes("a")
-                })
-            }
-        },
-        mounted() {
-            axios.get('https://randomuser.me/api/?results=50')
-                .then(res => {
-                    //console.log(res.data.results);
-                    this.users = res.data.results;
-                })
-                .catch(err => {
-                    console.error(err);
-                })
-        },
-    }
+import EventBus from "../bus";
+import axios from "axios";
+export default {
+  name: "usuarios",
+  data() {
+    return {
+      users: [],
+      buscar: ''
+    };
+  },
+  computed: {
+    
+    update: function () {
+          return EventBus.$on('add-busqueda', (item) => {
+              console.log(item);
+              this.users.filter(user => {
+              return user.name.first.includes(item);
+      });
+          })
+      },
+  },
+  mounted() {
+    axios
+      .get("https://randomuser.me/api/?results=50")
+      .then(res => {
+        //console.log(res.data.results);
+        this.users = res.data.results;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+};
 </script>
 
 <style>
-    .tamano-tarjetas {
-        width: 500px;
-        max-width: 18rem;
-        height: 350px;
-    }
+.tamano-tarjetas {
+  width: 500px;
+  max-width: 18rem;
+  height: 350px;
+}
 </style>
